a6a3f5127417726cc28c65f66b861c3faa27774d
pratikquant <pratik.mungekar@quantiphi.com>
2020-05-02 17:30:41 +0530

    app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
   name = 'Angular';
   output:any;
+  url='http://back-service:5000';
   constructor(private http:HttpClient){
   
   }
   
   callApi(){
     console.log('Click Me');
-    this.http.get('http://back-service:5000/test').subscribe(response=>{
+    let header = new HttpHeaders().set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9')
+    .set('Access-Control-Allow-Origin','*')
+    //.set('Accept-Encoding',' gzip, deflate')
+    
+    this.http.get(this.url+'/',{headers:header}).subscribe(response=>{
       console.log(response);
       this.output= response;
     })
